6dcf3f5aaaf20834e06df8a5949d2a13e91c2ed3
Alan Gutierrez <alan@prettyrobots.com>
2018-11-25 07:32:25 -0600

    Rename `assert` to `okay`.

--- t/keyify.t.js
+++ t/keyify.t.js
@@ -1,17 +1,17 @@
 require('proof')(10, prove)
 
-function prove (assert) {
+function prove (okay) {
     var Keyify = require('..')
 
-    assert(Keyify.stringify('x'), '"x"', 'stringify scalar')
-    assert(Keyify.stringify({b:2,a:1}), '["object","a",1,"b",2]', 'stringify object')
-    assert(Keyify.stringify({b:2,a:1}) === Keyify.stringify({a:1,b:2}), 'stringify different key orders equal')
-    assert(Keyify.stringify(['array']), '["array","array"]', 'stringify array with keyword')
-    assert(Keyify.stringify({a:{a:1}}), '["object","a",["object","a",1]]', 'stringify nested object')
+    okay(Keyify.stringify('x'), '"x"', 'stringify scalar')
+    okay(Keyify.stringify({b:2,a:1}), '["object","a",1,"b",2]', 'stringify object')
+    okay(Keyify.stringify({b:2,a:1}) === Keyify.stringify({a:1,b:2}), 'stringify different key orders equal')
+    okay(Keyify.stringify(['array']), '["array","array"]', 'stringify array with keyword')
+    okay(Keyify.stringify({a:{a:1}}), '["object","a",["object","a",1]]', 'stringify nested object')
 
-    assert(Keyify.parse(Keyify.stringify('x')), 'x', 'round-trip scalar')
-    assert(Keyify.parse(Keyify.stringify(['x'])), ['x'], 'round-trip array')
-    assert(Keyify.parse(Keyify.stringify({b:2,a:1})), {b:2,a:1}, 'round-trip object')
-    assert(Keyify.parse(Keyify.stringify(['array'])), ['array'], 'round-trip array with keyword')
-    assert(Keyify.parse(Keyify.stringify({a:{a:1}})), {a:{a:1}}, 'round-trip nested object')
+    okay(Keyify.parse(Keyify.stringify('x')), 'x', 'round-trip scalar')
+    okay(Keyify.parse(Keyify.stringify(['x'])), ['x'], 'round-trip array')
+    okay(Keyify.parse(Keyify.stringify({b:2,a:1})), {b:2,a:1}, 'round-trip object')
+    okay(Keyify.parse(Keyify.stringify(['array'])), ['array'], 'round-trip array with keyword')
+    okay(Keyify.parse(Keyify.stringify({a:{a:1}})), {a:{a:1}}, 'round-trip nested object')
 }
--- t/require.t.js
+++ t/require.t.js
@@ -1,5 +1,5 @@
 require('proof')(1, prove)
 
-function prove (assert) {
-    assert(require('..'), 'require')
+function prove (okay) {
+    okay(require('..'), 'require')
 }
