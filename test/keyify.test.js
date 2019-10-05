0f3901455fd367c15dfbee5d1bee675d99b943b5
Alan Gutierrez <alan@prettyrobots.com>
2019-10-04 22:32:10 -0500

    Revert Keyify test to Proof.
    See #13.

--- test/keyify.test.js
+++ test/keyify.test.js
@@ -1,17 +1,14 @@
-describe('keyify', () => {
+require('proof')(10, (okay) => {
     const Keyify = require('..')
-    const assert = require('assert')
-    it('can stringify and parse', () => {
-        assert.equal(Keyify.stringify('x'), '"x"', 'stringify scalar')
-    assert.equal(Keyify.stringify({b:2,a:1}), '["object","a",1,"b",2]', 'stringify object')
-    assert(Keyify.stringify({b:2,a:1}) === Keyify.stringify({a:1,b:2}), 'stringify different key orders equal')
-    assert.equal(Keyify.stringify(['array']), '["array","array"]', 'stringify array with keyword')
-    assert.equal(Keyify.stringify({a:{a:1}}), '["object","a",["object","a",1]]', 'stringify nested object')
+    okay(Keyify.stringify('x'), '"x"', 'stringify scalar')
+    okay(Keyify.stringify({b:2,a:1}), '["object","a",1,"b",2]', 'stringify object')
+    okay(Keyify.stringify({b:2,a:1}) === Keyify.stringify({a:1,b:2}), 'stringify different key orders equal')
+    okay(Keyify.stringify(['array']), '["array","array"]', 'stringify array with keyword')
+    okay(Keyify.stringify({a:{a:1}}), '["object","a",["object","a",1]]', 'stringify nested object')
 
-    assert.equal(Keyify.parse(Keyify.stringify('x')), 'x', 'round-trip scalar')
-    assert.deepStrictEqual(Keyify.parse(Keyify.stringify(['x'])), ['x'], 'round-trip array')
-    assert.deepStrictEqual(Keyify.parse(Keyify.stringify({b:2,a:1})), {b:2,a:1}, 'round-trip object')
-    assert.deepStrictEqual(Keyify.parse(Keyify.stringify(['array'])), ['array'], 'round-trip array with keyword')
-    assert.deepStrictEqual(Keyify.parse(Keyify.stringify({a:{a:1}})), {a:{a:1}}, 'round-trip nested object')
-    })
+    okay(Keyify.parse(Keyify.stringify('x')), 'x', 'round-trip scalar')
+    okay(Keyify.parse(Keyify.stringify(['x'])), ['x'], 'round-trip array')
+    okay(Keyify.parse(Keyify.stringify({b:2,a:1})), {b:2,a:1}, 'round-trip object')
+    okay(Keyify.parse(Keyify.stringify(['array'])), ['array'], 'round-trip array with keyword')
+    okay(Keyify.parse(Keyify.stringify({a:{a:1}})), {a:{a:1}}, 'round-trip nested object')
 })
